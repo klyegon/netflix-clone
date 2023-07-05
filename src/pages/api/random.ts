2c0d3867e3f15427b0ff3b3929f94f9ee60c457c
okxle <83871013+okxle@users.noreply.github.com>
2023-07-05 22:33:00 +0800

    fix: add billboard

---
+++ src/pages/api/random.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from "@/lib/primadb";
+import serverAuth from "@/lib/serverAuth";
+
+export default async function handler(request: NextApiRequest, response: NextApiResponse) {
+    if (request.method != "GET") return response.status(405).end();
+    try {
+        await serverAuth(request);
+
+        const movieCount = await prismadb.movie.count();
+        const randomIndex = Math.floor(Math.random() * movieCount);
+
+        const randomMovies = await prismadb.movie.findMany({
+            take: 1,
+            skip: randomIndex
+        })
+
+        return response.status(200).json(randomMovies[0]);
+    } catch (error) {
+        console.log(error);
+        return response.status(400).end();
+    }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import Navbar from "@/components/Navbar"
+import Billboard from "@/components/Billboard";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -24,6 +25,7 @@ export default function Home() {
   return (
     <div>
       <Navbar />
+      <Billboard />
     </div>
   );
 }
